d0e5e52cc7dba1f0530a2c3009ad903f2a4c9869
Nissan Dookeran <nissan.dookeran@gmail.com>
2018-06-13 13:39:23 +1000

    use Brady as logo, Homepage CTA changed to point to correct path files

--- sampleApp/ClientApp/src/components/templates/HomePageTemplate.tsx
+++ sampleApp/ClientApp/src/components/templates/HomePageTemplate.tsx
@@ -1,6 +1,6 @@
 
 import * as React from 'react';
-import logo from '../../assets/images/logo.svg';
+import logo from '../../assets/images/Brady.svg';
 
 class HomePageTemplate extends React.Component {
     public render() {
@@ -8,10 +8,20 @@ class HomePageTemplate extends React.Component {
             <div className="App">
                 <header className="App-header">
                     <img src={logo} className="App-logo" alt="logo" />
-                    <h1 className="App-title">Welcome to React</h1>
+                    <h1 className="App-title">Welcome to Reddi React on .NET, powered by TypeScript</h1>
                 </header>
                 <p className="App-intro">
-                    To get started, edit <code>src/App.tsx</code> and save to reload.
+                    To get started, review the tests in
+                    <ul>
+                        <li><code>src/tests/App.test.tsx</code></li>
+                        <li><code>src/routes/Routes.test.tsx</code></li>
+                    </ul>
+                    then explore their targets at
+                    <ul>
+                        <li><code>src/components/App.tsx</code></li>
+                        <li><code>src/routes/Routes.tsx</code></li>
+                    </ul>
+                    and save to reload.
 </p>
             </div>
         );
