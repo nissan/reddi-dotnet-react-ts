dc38db6ea12c3089e76d073c8d93a45ba1c615fe
Nissan Dookeran <nissan.dookeran@gmail.com>
2018-06-13 13:56:28 +1000

    add boostrap

--- sampleApp/ClientApp/src/components/templates/HomePageTemplate.tsx
+++ sampleApp/ClientApp/src/components/templates/HomePageTemplate.tsx
@@ -5,24 +5,39 @@ import logo from '../../assets/images/Brady.svg';
 class HomePageTemplate extends React.Component {
     public render() {
         return (
-            <div className="App">
-                <header className="App-header">
-                    <img src={logo} className="App-logo" alt="logo" />
-                    <h1 className="App-title">Welcome to Reddi React on .NET, powered by TypeScript</h1>
-                </header>
-                <p className="App-intro">
-                    To get started, review the tests in
-                    <ul>
-                        <li><code>src/tests/App.test.tsx</code></li>
-                        <li><code>src/routes/Routes.test.tsx</code></li>
-                    </ul>
-                    then explore their targets at
-                    <ul>
-                        <li><code>src/components/App.tsx</code></li>
-                        <li><code>src/routes/Routes.tsx</code></li>
-                    </ul>
-                    and save to reload.
-</p>
+            <div className="App container">
+                <div className="row">
+                    <div className="col">
+                        <header className="App-header">
+                            <img src={logo} className="App-logo" alt="logo" />
+                            <h1 className="App-title">Welcome to Reddi React </h1>
+                        </header>
+
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="col">
+                        <h2 className="display-5">Leveraging .NET Core 2.1, TypeScript and Bootstrap</h2>
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="col">
+
+                        <div className="App-intro jumbotron">
+                            To get started, review the tests in
+                            <ul className="list-group">
+                                <li className="list-group-item"><code>src/tests/App.test.tsx</code></li>
+                                <li className="list-group-item"><code>src/routes/Routes.test.tsx</code></li>
+                            </ul>
+                            then explore their targets at
+                            <ul className="list-group">
+                                <li className="list-group-item"><code>src/components/App.tsx</code></li>
+                                <li className="list-group-item"><code>src/routes/Routes.tsx</code></li>
+                            </ul>
+                            and save to reload.
+                        </div>
+                    </div>
+                </div>
             </div>
         );
     }
